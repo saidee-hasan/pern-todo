556be51db992f53127d4d2bd315b5c5529a91be4
saidee-hasan <mdsaideehasan@gmail.com>
2025-06-02 11:10:01 +0600

    pdated

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
--- Active: 1748790932367@@127.0.0.1@5432@perntodo
+-- Active: 1748836110132@@dpg-d0uilu63jp1c7384nnug-a.singapore-postgres.render.com@5432@pern_todo_server
 -- In psql or PgAdmin
 
 CREATE DATABASE perntodo;
--- db.js
+++ db.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  password: "778899",
-  host: "localhost",
-  port: 5432,
-  database: "perntodo",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
